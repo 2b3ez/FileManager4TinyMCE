af6bbd7f1d34c288b62face954270c9708e39f2a
Frédéric <mybeeez@gmail.com>
2013-04-29 22:59:48 +0200

    Plugin FileManager for TinyMCE 4
    Upload and manage your file easily
    Plugin create with jquery, bootstrap, and FineUploader in open source
    licence.

---
+++ tinymce/plugins/filemanager/lang/en_EN.php
@@ -0,0 +1,12 @@
+<?
+define('lang_Select','Select');
+define('lang_Erase','Erase');
+define('lang_Confirm_del','Are you sure you want to delete this file?');
+define('lang_All','All');
+define('lang_Files','Files');
+define('lang_Images','Images');
+define('lang_Archives','Archives');
+define('lang_Error_Upload','The uploaded file exceeds the max size allowed.');
+define('lang_Error_extension','File extension is not allowed.');
+define('lang_Upload_file','Upload a file');
+?>
---
+++ tinymce/plugins/filemanager/lang/fr_FR.php
@@ -0,0 +1,12 @@
+<?
+define('lang_Select','Sélectionner');
+define('lang_Erase','Effacer');
+define('lang_Confirm_del','Êtes-vous sûr de vouloir effacer ce fichier ?');
+define('lang_All','Tous');
+define('lang_Files','Fichiers');
+define('lang_Images','Images');
+define('lang_Archives','Archives');
+define('lang_Error_Upload', 'Votre fichier dépasse la taille maximum autorisée.');
+define('lang_Error_extension','Extension de fichier non autorisée');
+define('lang_Upload_file','Envoyer un fichier');
+?>
