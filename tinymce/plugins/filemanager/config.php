82b85752efc9cf013e698d2533c788816a953262
Frédéric <mybeeez@gmail.com>
2013-05-01 20:43:39 +0200

    Revert "Plugin FileManager for TinyMCE 4"
    This reverts commit af6bbd7f1d34c288b62face954270c9708e39f2a.

--- tinymce/plugins/filemanager/config.php
+++
@@ -1,7 +0,0 @@
-<?
-
-$upload_dir = '../../../../../upload/'; // dir for upload file
-$MaxSizeUpload=1000000; // 1Mo
-$ext=array('png','jpg','jpeg','gif','pdf','zip','rar','doc','docx','xls','xlsx','txt','csv'); //extensions allowed
-
-?>
