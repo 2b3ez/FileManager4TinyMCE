aeeed4cf2cd40ebe394828002bab984c24075aa3
Ramil <r@me0.biz>
2013-05-02 14:57:05 +0400

    Update config.php

--- tinymce/plugins/filemanager/config.php
+++ tinymce/plugins/filemanager/config.php
@@ -1,6 +1,7 @@
 <?
-
-$upload_dir = '../../../../../upload/'; // dir for upload file
+$root = rtrim($_SERVER['DOCUMENT_ROOT'],'/'); // Root path for PHP
+$wwwroot = ''; // Root path for frontend
+$upload_dir = '/source/'; // dir for upload file
 $MaxSizeUpload=1000000; // 1Mo
 $ext=array('png','jpg','jpeg','gif','pdf','zip','rar','doc','docx','xls','xlsx','txt','csv'); //extensions allowed
 
