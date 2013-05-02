96b421b26f85945282a29cec41ec0d5e83059dc0
Ramil <r@me0.biz>
2013-05-02 17:00:24 +0400

    Updates
    Move $ext_img, $ext_file, $ext_misc to config.php

--- tinymce/plugins/filemanager/config.php
+++ tinymce/plugins/filemanager/config.php
@@ -1,8 +1,14 @@
 <?
-$root = rtrim($_SERVER['DOCUMENT_ROOT'],'/'); // Root path for PHP
-$wwwroot = ''; // Root path for frontend
+$root = rtrim($_SERVER['DOCUMENT_ROOT'],'/');
+
+$wwwroot = '';
 $upload_dir = '/source/'; // dir for upload file
 $MaxSizeUpload=1000000; // 1Mo
 $ext=array('png','jpg','jpeg','gif','pdf','zip','rar','doc','docx','xls','xlsx','txt','csv'); //extensions allowed
+
+// extensions for filemanager
+$ext_img = array('jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff');
+$ext_file = array('doc', 'docx', 'pdf', 'xls', 'xlsx', 'txt', 'csv');
+$ext_misc = array('zip', 'rar');
 
 ?>
