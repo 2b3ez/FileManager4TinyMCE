67524c4eb8b90adb25c399e5264936a2af8dd950
2b3ez <mybeeez@gmail.com>
2013-06-28 17:02:41 +0200

    Update fr_FR.php

--- tinymce/plugins/filemanager/lang/fr_FR.php
+++ tinymce/plugins/filemanager/lang/fr_FR.php
@@ -1,7 +1,7 @@
 <?php
 define('lang_Select','Sélectionner');
 define('lang_Erase','Effacer');
-define('lang_Open','Ouvre');
+define('lang_Open','Ouvrir');
 define('lang_Confirm_del','Êtes-vous sûr de vouloir effacer ce fichier ?');
 define('lang_All','Tous');
 define('lang_Files','Fichiers');
@@ -16,11 +16,10 @@ define('lang_Music','Musique');
 define('lang_New_Folder','Nouveau dossier');
 define('lang_Folder_Created','Dossier correctement créé');
 define('lang_Existing_Folder','Dossier existant');
-define('lang_Confirm_Folder_del','Êtes-vous sûr de vouloir supprimer le dossier et tous les éléments en elle?');
+define('lang_Confirm_Folder_del','Êtes-vous sûr de vouloir supprimer le dossier ainsi que tous ses éléments ?');
 define('lang_Return_Files_List','Revenir à la liste des fichiers');
-define('lang_Preview','Avant-première');
+define('lang_Preview','Aperçu');
 define('lang_Download','Download');
 define('lang_Insert_Folder_Name','Insérer le nom du dossier:');
-define('lang_Root','base');
-
+define('lang_Root','racine');
 ?>
